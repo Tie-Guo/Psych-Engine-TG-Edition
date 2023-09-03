202e2b31a99897f4f0bd10d4875d8c0abd7db361
Tie_Guo <117576846+Tie-Guo@users.noreply.github.com>
2023-09-03 20:27:44 +0800

    Add files via upload

--- source/backend/CustomFadeTransition.hx
+++ source/backend/CustomFadeTransition.hx
@@ -7,8 +7,8 @@ class CustomFadeTransition extends MusicBeatSubstate {
 	private var leTween:FlxTween = null;
 	public static var nextCamera:FlxCamera;
 	var isTransIn:Bool = false;
-	var transBlack:FlxSprite;
-	var transGradient:FlxSprite;
+	var loadBG:FlxSprite;
+	var loadTX:FlxText;
 
 	public function new(duration:Float, isTransIn:Bool) {
 		super();
@@ -17,32 +17,23 @@ class CustomFadeTransition extends MusicBeatSubstate {
 		var zoom:Float = FlxMath.bound(FlxG.camera.zoom, 0.05, 1);
 		var width:Int = Std.int(FlxG.width / zoom);
 		var height:Int = Std.int(FlxG.height / zoom);
-		transGradient = FlxGradient.createGradientFlxSprite(1, height, (isTransIn ? [0x0, FlxColor.BLACK] : [FlxColor.BLACK, 0x0]));
-		transGradient.scale.x = width;
-		transGradient.updateHitbox();
-		transGradient.scrollFactor.set();
-		add(transGradient);
-
-		transBlack = new FlxSprite().makeGraphic(1, height + 400, FlxColor.BLACK);
-		transBlack.scale.x = width;
-		transBlack.updateHitbox();
-		transBlack.scrollFactor.set();
-		add(transBlack);
-
-		transGradient.x -= (width - FlxG.width) / 2;
-		transBlack.x = transGradient.x;
+		
+		loadBG = new FlxSprite().loadGraphic(Paths.image('menus/loadingScreen' FlxG.random.int(1, 2)));
+		add(loadBG);
+		if (isTransIn) loadBG.alpha = 0;
+		
+		loadTX = new FlxText(50, 200, 0, 'Loading... \nWait it...', 50);
+		loadTX.setFormat(Paths.font('vcr.ttf'), 50, FlxColor.WHITE);
+		add(loadTX);
 
 		if(isTransIn) {
-			transGradient.y = transBlack.y - transBlack.height;
-			FlxTween.tween(transGradient, {y: transGradient.height + 50}, duration, {
+			FlxTween.tween(loadBG, {alpha: 1}, duration, {
 				onComplete: function(twn:FlxTween) {
 					close();
 				},
 			ease: FlxEase.linear});
 		} else {
-			transGradient.y = -transGradient.height;
-			transBlack.y = transGradient.y - transBlack.height + 50;
-			leTween = FlxTween.tween(transGradient, {y: transGradient.height + 50}, duration, {
+			leTween = FlxTween.tween(loadBG, {alpha: 0}, duration, {
 				onComplete: function(twn:FlxTween) {
 					if(finishCallback != null) {
 						finishCallback();
@@ -52,24 +43,10 @@ class CustomFadeTransition extends MusicBeatSubstate {
 		}
 
 		if(nextCamera != null) {
-			transBlack.cameras = [nextCamera];
-			transGradient.cameras = [nextCamera];
+			loadBG.cameras = [nextCamera];
+			loadTX.cameras = [nextCamera];
 		}
 		nextCamera = null;
-	}
-
-	override function update(elapsed:Float) {
-		if(isTransIn) {
-			transBlack.y = transGradient.y + transGradient.height;
-		} else {
-			transBlack.y = transGradient.y - transBlack.height;
-		}
-		super.update(elapsed);
-		if(isTransIn) {
-			transBlack.y = transGradient.y + transGradient.height;
-		} else {
-			transBlack.y = transGradient.y - transBlack.height;
-		}
 	}
 
 	override function destroy() {
