50dc09506e1a932286200d80bc15261eb4337c05
Tie_Guo <117576846+Tie-Guo@users.noreply.github.com>
2023-09-03 18:29:16 +0800

    Add files via upload

--- source/states/TitleState.hx
+++ source/states/TitleState.hx
@@ -144,13 +144,12 @@ class TitleState extends MusicBeatState
 		{
 			StoryMenuState.weekCompleted = FlxG.save.data.weekCompleted;
 		}
-		
-		FlxTransitionableState.skipNextTransIn = true;
-		FlxTransitionableState.skipNextTransOut = true;
 
 		FlxG.mouse.visible = false;
 
 		if(FlxG.save.data.flashing == null && !FlashingState.leftState) {
+			FlxTransitionableState.skipNextTransIn = true;
+			FlxTransitionableState.skipNextTransOut = true;
 			MusicBeatState.switchState(new FlashingState());
 		} else {
 			startCutscenesIn();
