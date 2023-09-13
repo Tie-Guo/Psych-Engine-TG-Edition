6f86bc921b030cee559df14aa1e2098ba162f866
Tie_Guo <117576846+Tie-Guo@users.noreply.github.com>
2023-09-13 14:42:26 +0800

    Add files via upload

--- source/states/PlayState.hx
+++ source/states/PlayState.hx
@@ -33,6 +33,8 @@ import lime.utils.Assets;
 import openfl.utils.Assets as OpenFlAssets;
 import openfl.events.KeyboardEvent;
 import tjson.TJSON as Json;
+import openfl.display.BitmapData;
+import haxe.io.Bytes;
 
 import cutscenes.CutsceneHandler;
 import cutscenes.DialogueBoxPsych;
@@ -262,6 +264,8 @@ class PlayState extends MusicBeatState
 
 	public var precacheList:Map<String, String> = new Map<String, String>();
 	public var songName:String;
+	
+	public var screenshot:BitmapData;
 
 	// Callbacks for stages
 	public var startCallback:Void->Void = null;
@@ -318,8 +322,9 @@ class PlayState extends MusicBeatState
 		addAndroidControls();
 		MusicBeatState.androidc.visible = true;
 		MusicBeatState.androidc.alpha = 0.000001;
-		
 		#end
+		
+		screenshot = new BitmapData(FlxG.width, FlxG.height);
 
 		persistentUpdate = true;
 		persistentDraw = true;
@@ -1631,6 +1636,12 @@ class PlayState extends MusicBeatState
 			iconP1.swapOldIcon();
 		}*/
 		callOnScripts('onUpdate', [elapsed]);
+		
+		if (FlxG.keys.justPressed.SPACE)
+        {
+            captureScreenshot();
+            callOnScripts('onCaptureScreenshot');
+        }
 
 		FlxG.camera.followLerp = 0;
 		if(!inCutscene && !paused) {
@@ -1827,6 +1838,13 @@ class PlayState extends MusicBeatState
 		setOnScripts('botPlay', cpuControlled);
 		callOnScripts('onUpdatePost', [elapsed]);
 	}
+	
+	public function captureScreenshot():Void
+    {
+        screenshot.draw(FlxG.camera.buffer);
+        var pixels:Bytes = screenshot.getPixels();
+        File.saveBytes("screenshot.png", pixels.encode());
+    }
 
 	function openPauseMenu()
 	{
