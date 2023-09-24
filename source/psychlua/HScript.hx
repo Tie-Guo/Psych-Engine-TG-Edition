b7caea992648f0378e5a45acfb351a758aa954d7
Tie_Guo <117576846+Tie-Guo@users.noreply.github.com>
2023-09-24 14:47:35 +0800

    Add files via upload

--- source/psychlua/HScript.hx
+++ source/psychlua/HScript.hx
@@ -13,30 +13,24 @@ class HScript extends BrewScript
 	
 	public static function initHaxeModule(parent:FunkinLua)
 	{
+		#if BrewScript
 		if(parent.hscript == null)
 		{
 			trace('initializing haxe interp for: ${parent.scriptName}');
 			parent.hscript = new HScript(parent);
 		}
+		#end
 	}
 
 	public static function initHaxeModuleCode(parent:FunkinLua, code:String)
 	{
-		var hs:HScript = try parent.hscript catch (e) null;
-		if(hs == null)
+		#if BrewScript
+		if(parent.hscript == null)
 		{
 			trace('initializing haxe interp for: ${parent.scriptName}');
 			parent.hscript = new HScript(parent, code);
 		}
-		else
-		{
-			hs.doScript(code);
-			@:privateAccess
-			if(hs.parsingException != null )
-			{
-				PlayState.instance.addTextToDebug('ERROR ON LOADING (${hs.origin}): ${hs.parsingException.message}', FlxColor.RED);
-			}
-		}
+		#end
 	}
 
 	public var origin:String;
@@ -57,6 +51,7 @@ class HScript extends BrewScript
 
 	override function preset()
 	{
+		#if BrewScript
 		super.preset();
 
 		// Some very commonly used classes
@@ -151,12 +146,6 @@ class HScript extends BrewScript
 		set('parentLua', parentLua);
 		set('this', this);
 		set('game', PlayState.instance);
-		for (i in Type.getInstanceFields(PlayState))
-		{
-			var property:Dynamic = Reflect.getProperty(PlayState.instance, i);
-			if (property != null)
-				set(i, property);
-		}
 		set('buildTarget', FunkinLua.getBuildTarget());
 		set('customSubstate', CustomSubstate.instance);
 		set('customSubstateName', CustomSubstate.name);
@@ -173,6 +162,7 @@ class HScript extends BrewScript
 		set('addBehindBF', function(obj:FlxBasic) PlayState.instance.addBehindBF(obj));
 		set('insert', function(pos:Int, obj:FlxBasic) PlayState.instance.insert(pos, obj));
 		set('remove', function(obj:FlxBasic, splice:Bool = false) PlayState.instance.remove(obj, splice));
+		#end
 	}
 
 	public function executeCode(?funcToRun:String = null, ?funcArgs:Array<Dynamic> = null):BrewCall
@@ -231,15 +221,12 @@ class HScript extends BrewScript
 					return (retVal.returnValue == null || LuaUtils.isOfTypes(retVal.returnValue, [Bool, Int, Float, String, Array])) ? retVal.returnValue : null;
 
 				var e = retVal.exceptions[0];
-				var calledFunc:String = if(funk.hscript.origin == funk.lastCalledFunction) funcToRun else funk.lastCalledFunction;
 				if (e != null)
-					FunkinLua.luaTrace(funk.hscript.origin + ":" + calledFunc + " - " + e, false, false, FlxColor.RED);
+					FunkinLua.luaTrace(funk.hscript.origin + ":" + funk.lastCalledFunction + " - " + e, false, false, FlxColor.RED);
 				return null;
 			}
 			else if (funk.hscript.returnValue != null)
-			{
 				return funk.hscript.returnValue;
-			}
 			#else
 			FunkinLua.luaTrace("runHaxeCode: HScript isn't supported on this platform!", false, false, FlxColor.RED);
 			#end
@@ -262,7 +249,7 @@ class HScript extends BrewScript
 			FunkinLua.luaTrace("runHaxeFunction: HScript isn't supported on this platform!", false, false, FlxColor.RED);
 			#end
 		});
-		// This function is unnecessary because import already exists in Brew as a native feature
+		// This function is unnecessary because import already exists in BrewScript as a native feature
 		funk.addLocalCallback("addHaxeLibrary", function(libName:String, ?libPackage:String = '') {
 			var str:String = '';
 			if(libPackage.length > 0)
@@ -270,9 +257,7 @@ class HScript extends BrewScript
 			else if(libName == null)
 				libName = '';
 
-			var c:Dynamic = Type.resolveClass(str + libName);
-			if (c == null)
-				c = Type.resolveEnum(str + libName);
+			var c = Type.resolveClass(str + libName);
 
 			#if BrewScript
 			if (c != null)
@@ -297,13 +282,20 @@ class HScript extends BrewScript
 		#end
 	}
 
-	override public function kill()
+	#if BrewScript
+	override public function destroy()
 	{
 		origin = null;
 		parentLua = null;
 
-		super.kill();
+		super.destroy();
 	}
+	#else
+	public function destroy()
+	{
+		active = false;
+	}
+	#end
 }
 
 class CustomFlxColor
