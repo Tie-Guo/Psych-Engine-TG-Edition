5d7ae8e85eb746db64ea79f71265c75c80332fe2
Tie_Guo <117576846+Tie-Guo@users.noreply.github.com>
2023-09-29 10:52:33 +0800

    Add files via upload

--- source/backend/Language.hx
+++ source/backend/Language.hx
@@ -4,9 +4,9 @@ class Language {
 	public static var languages:Array<String> = ['English', 'Chinese'];
 	public static var defaultLanguage:String = 'English';
 	
-	public static function font()
+	public function font()
 	{
-		if (ClientPrefs.data.language != 'English')
+		if (ClientPrefs.data.language == 'English')
 			return Paths.font('vcr.ttf');
 		else
 			return Paths.font('syht.ttf');
